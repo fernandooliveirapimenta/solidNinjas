31694edb4f66496b4c9f9a73f9f40216034a4837
Fernando Pimenta <fernando.pimenta107@gmail.com>
2019-02-16 13:45:57 -0200

    primeiro commit

---
+++ src/main/java/com/fernando/SolidNinjasApplication.java
@@ -0,0 +1,14 @@
+package com.fernando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidNinjasApplication.class, args);
+	}
+
+}
+
